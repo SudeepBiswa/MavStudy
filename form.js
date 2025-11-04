3f57f4907dacdc8aebd5cee1ac28086c9d8c8aff
SudeepBiswa <sudeepbiswa@gmail.com>
2025-11-03 19:54:59 -0600

    fix login and signup, and add datasave

--- form.js
+++ form.js
@@ -1,11 +1,23 @@
 import { auth, db } from "./firebase-init.js";
-import { createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { addDoc, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { addDoc, collection, serverTimestamp, setDoc, doc, getFirestore} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 //text
 const messageHandler = document.getElementById("messageHandler");
 
 const createButton = document.getElementById("createButton");
+
+ const ProfileData = {
+            InitialAccountCreationComplete: false,
+            firstName: "",
+            lastName: "",
+            major: "",
+            grade: "",
+            age: null,
+            enrollmentStatus: "",
+            groupSize: "",
+            accountCreationDate: new Date()
+        };
 
 //buttons
 createButton.addEventListener("click", async (event) => {
@@ -15,25 +27,33 @@ createButton.addEventListener("click", async (event) => {
     const password = document.getElementById("password").value.trim();
 
     try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-
-        if (!(user.email.endsWith("@unomaha.edu") || user.email.endsWith("@nebraska.edu"))) {
-            messageHandler.style.color = "red";
-            messageHandler.textContent = "Please use a valid UNO or Nebraska email.";
-            return;
-        }
-
-        messageHandler.style.color = "green";
-        messageHandler.textContent = "Account created";
-        //login registry
-        await addEvent(user, "signUp");
-
-        setTimeout(() => { window.location.href = "profile.html"; }, 1000);
-    } catch (error) {
+    if (!(email.endsWith("@unomaha.edu") || email.endsWith("@nebraska.edu"))) {
         messageHandler.style.color = "red";
-        messageHandler.textContent = error.message;
+        messageHandler.textContent = "Please use a valid UNO or Nebraska email.";
+        return;
     }
+
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    
+   
+        
+        
+    await setDoc(doc(db, "users", user.uid), {ProfileData});
+
+    messageHandler.style.color = "green";
+    messageHandler.textContent = "Account created";
+    
+    // log signup event
+    //await addEvent(user, "signUp");
+
+    setTimeout(() => { window.location.href = "profile.html"; }, 1000);
+
+} catch (error) {
+    messageHandler.style.color = "red";
+    messageHandler.textContent = error.message;
+}
+
 });
 
 //right now this button works to sign out the current user
@@ -41,12 +61,50 @@ const loginButton = document.getElementById("loginButton");
 loginButton.addEventListener("click", async (event) => {
     event.preventDefault();
 
-    try {
-        await signOut(auth);
-        console.log("Signed out successfully");
-    } catch (error) {
-        console.error("Error signing out:", error);
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    //login feature
+    if (!email || !password){
+        messageHandler.textContent = "Please enter both email and password!";
+        messageHandler.style.color = "red";
+        return;
     }
+
+    try{
+        const userCred = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCred.user;
+        const ref = await getDoc(doc(db, "users", user.uid));
+        if(!ref){
+            messageHandler.textContent = "UserData DOES NOT EXIST: CREATING USERDATA!!";
+            messageHandler.style.color = "yellow";
+            setTimeout(() => {
+                messageHandler.textContent = "USERDATA CREATED!!";
+                messageHandler.style.color = "green";
+            }, 1000)
+            await setDoc(ref, {ProfileData});
+           
+        }
+
+        messageHandler.textContent = "Login Successful!"
+        messageHandler.style.color = "green";
+
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1000);
+    }
+    catch (error){
+        messageHandler.textContent = error.message;
+        messageHandler.style.color = "red";
+    }
+   //logout feature
+    // try {
+    //     await signOut(auth);
+    //     console.log("Signed out successfully");
+    // } catch (error) {
+    //     console.error("Error signing out:", error);
+    // }
+
 });
 
 //creates collection for user events with timestamp
--- script.js
+++ script.js
@@ -1,23 +1,70 @@
 import { auth, db } from "./firebase-init.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { doc, setDoc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("userDetailsForm");
     const messageHandler = document.getElementById("messageHandler");
+    const submitBtn = document.getElementById("saveUserDetailsButton")
+
+    const convo = document.getElementById("convo");
+    const chat =  document.getElementById("chatty")
+
+    const lFirstName = document.getElementById("userFirstName");
+    const lLastName = document.getElementById("userLastName");
+    const lMajor = document.getElementById("majorDropdown");
+    const lGrade = document.getElementById("gradeDropdown");
+    const lAge = document.getElementById("ageInput");
+    const lEnrollment = document.getElementById("enrollmentStatusDropdown");
+    const lGroupSize = document.getElementById("groupSizeDropdown");
+
 
     // if user is NOT authenticated redirect to login page
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async(user) => {
+        //let path = window.location.href
+        //let page = path.substring(path.lastIndexOf("/") + 1) || 'nil';
+       
         if (!user) {
-            window.location.href = "login.html";
+            //if (page != "login.html"){
+                window.location.href = "login.html";
+           // }
             return;
         }
 
+        const dataRef = await getDoc(doc(db, "users", user.uid));
+        const userData = dataRef.data();
+
+        //console.log(user.uid)
+        //console.log(userData.ProfileData.firstName)
+
+        if(!userData.ProfileData.InitialAccountCreationComplete){
+            submitBtn.value = "Create Profile"; 
+            convo.style.visibility = "hidden";
+            chatty.style.visibility = "hidden";
+            
+        }
+        else{
+            lFirstName.value = userData.ProfileData.firstName
+            lFirstName.readOnly = true;
+            lLastName.value = userData.ProfileData.lastName
+            lLastName.readOnly = true;
+            lMajor.value = userData.ProfileData.major
+            lGrade.value = userData.ProfileData.grade
+            lAge.value = userData.ProfileData.age
+            lEnrollment.value = userData.ProfileData.enrollmentStatus
+            lGroupSize.value = userData.ProfileData.groupSize
+        }
+
+       
+        
         //stores user details from form
         form.addEventListener("submit", async (event) => {
         event.preventDefault();
 
+        
+
         const studentData = {
+            InitialAccountCreationComplete: true,
             firstName: document.getElementById("userFirstName").value.trim(),
             lastName: document.getElementById("userLastName").value.trim(),
             major: document.getElementById("majorDropdown").value,
@@ -28,9 +75,21 @@ document.addEventListener("DOMContentLoaded", () => {
         };
         
         try {
-            await setDoc(doc(db, "users", user.uid), studentData);
+            let canGoToNextPage = false;
+            if(!userData.ProfileData.InitialAccountCreationComplete){
+                canGoToNextPage = true;
+            }
+            await updateDoc(doc(db, "users", user.uid), {ProfileData: studentData});
             messageHandler.style.color = "green";
-            messageHandler.textContent = "User details saved successfully!";
+            if(canGoToNextPage){
+                messageHandler.textContent = "PROFILE CREATED!!";
+                setTimeout(() => {
+                    window.location.href = "index.html";
+                }, 1000);
+            }
+            else{
+                messageHandler.textContent = "User details saved successfully!";
+            }
         } catch (error) {
             messageHandler.style.color = "red";
             messageHandler.textContent = error.message;
